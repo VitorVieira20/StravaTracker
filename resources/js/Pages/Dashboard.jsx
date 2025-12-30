3ac100de4810e6a7b0cc89390a75f81446d66378
VitorVieira20 <vitor20vieira2@gmail.com>
2025-12-30 12:19:00 +0000

    feat: add React and Inertia

---
+++ resources/js/Pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+
+    return (
+        <div className="flex justify-center items-center text-blue-500 text-9xl">
+            Boas Mano
+        </div>
+    );
+}
---
+++ resources/views/app.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-PT">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Vitor Vieira Web Developer">
+
+    <meta name="theme-color" content="#ffffff">
+    <link rel="preload" as="style"
+        href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&display=swap">
+
+    <link rel="canonical" href="{{ url()->current() }}" />
+    <link rel="preload" as="style"
+        href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&display=swap">
+    <link rel="canonical" href="{{ url()->current() }}" />
+    <link rel="preload" as="style"
+        href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&display=swap">
+
+    <title>Strava Tracker</title>
+
+    @viteReactRefresh
+    @routes
+    @if (app()->environment('local') || app()->environment('production'))
+        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx", "resources/css/app.css"])
+    @endif
+    @inertiaHead
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <style>
+        * {
+            font-family: "Poppins", sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    @inertia
+</body>
+
+</html>
